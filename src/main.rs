6443047b9bd86a53be25dc77b7d632a3e0a5ddfa
Ben Morrison <ben@gbmor.dev>
2019-09-11 03:41:24 -0400

    rltk setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rltk::{Console, GameState, Rltk};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, context: &mut Rltk) {
+        context.cls();
+        context.print(1, 1, "MORTAL WOMBAT MFERS");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let context =
+        Rltk::init_simple8x8(80, 50, "Mortal Wombat", "resources");
+    let gamestate = State {};
+    rltk::main_loop(context, gamestate);
 }
