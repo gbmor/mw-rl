db4a2ebc22c31f2b0293f917217c510d14b5aba3
Ben Morrison <ben@gbmor.dev>
2019-09-14 02:38:36 -0400

    solid walls

--- src/main.rs
+++ src/main.rs
@@ -124,23 +124,27 @@ fn main() {
 fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
     let mut posns = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     (&mut players, &mut posns).join().into_iter().for_each(
         |(_player, pos)| {
-            pos.x += dx;
-            pos.y += dy;
+            let dest_idx = xy_idx(pos.x + dx, pos.y + dy);
+            if map[dest_idx] != TileType::Wall {
+                pos.x += dx;
+                pos.y += dy;
 
-            if pos.x < 0 {
-                pos.x = 0;
-            }
-            if pos.x > 79 {
-                pos.x = 79;
-            }
-            if pos.y < 0 {
-                pos.y = 0;
-            }
-            if pos.y > 49 {
-                pos.y = 49;
+                if pos.x < 0 {
+                    pos.x = 0;
+                }
+                if pos.x > 79 {
+                    pos.x = 79;
+                }
+                if pos.y < 0 {
+                    pos.y = 0;
+                }
+                if pos.y > 49 {
+                    pos.y = 49;
+                }
             }
         },
     );
